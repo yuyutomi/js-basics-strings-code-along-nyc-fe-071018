7d377809eac700d12d3621b4e0fec537bb805585
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-09 23:43:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = 'Jane Doe'
-const welcomeMessage = "Welcome to Flatbook," + currentUser + 
+const welcomeMessage = "Welcome to Flatbook," + currentUser + '!'
