caf921bc49ad33e0763b405ed06f681bdba26b9a
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-10 00:07:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = 'Jane Doe';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
-cont excitedWelcomeMessage = welcomeMessage.toUpperase();
+cont excitedWelcomeMessage = welcomeMessage.toUpperCase();
