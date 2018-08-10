bfb2a3da7b6d4d022e2c5813d353ba24f87708a2
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-10 00:10:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const currentUser = 'Jane Doe';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser}.slice()`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}`;
